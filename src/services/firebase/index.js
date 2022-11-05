8552b5d125535c78c8c88ab7b280df8fabca2e2d
diego.tur <gpslinea15@hotmail.com>
2022-11-05 11:31:14 -0300

    env

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgOzAo8NA_hFgiiXH_K5BXbKYy5vN4Rk8",
-    authDomain: "gamefactory-3149a.firebaseapp.com",
-    projectId: "gamefactory-3149a",
-    storageBucket: "gamefactory-3149a.appspot.com",
-    messagingSenderId: "312425038152",
-    appId: "1:312425038152:web:1769782cb5894b32a6b37d"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
