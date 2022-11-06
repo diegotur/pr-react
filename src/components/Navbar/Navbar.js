6da8cda4f1cd325afbe3b444891ad636c8692dd8
diego.tur <gpslinea15@hotmail.com>
2022-11-06 12:22:09 -0300

    navBarRefactoredAndAsynched

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,33 @@
-               
 import './Navbar.css'
 import CartWidget from '../CartWidget/CartWidget'
 import logo from './assets/Logo.png'
 import { NavLink } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 import { getProductByCategory } from '../../services/firestore/products'
+import { useAsync } from '../../Hooks/useAsync'
 
 const Navbar = () => {
 
-    const [categories, setCategories] = useState([])
 
-    useEffect(() => {
+    const {data: categories, error, loading} = useAsync(getProductByCategory, [])
 
-        getProductByCategory().then(data => {
-            setCategories(data)
-        })
-    }, []) 
+    if (error){
+        <h1>Hubo Un error</h1>
+    }
+
+    if(loading) {
+        return <nav className = "Navbar">
+        <div className ="contLeft">
+        <NavLink to={'/'}><img src= {logo} alt="Logo" className="logo"></img></NavLink>
+        </div>
+        <div className="Categories">
+        </div>
+        <div></div>
+        <div>
+        <CartWidget />
+        </div>
+    </nav>
+
+    }
 
     return (
         <nav className = "Navbar">
